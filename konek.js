39b541b09d437b6e77b3fde7c82678119ef088c4
Fendy <fendy132us@yahoo.com>
2020-09-11 11:42:26 +0700

    menambahkan file server.js, konek.js

---
+++ konek.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql')
+
+var conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database: 'db_rest_api '
+})
+
+conn.connect((err) =>
+{
+   if(err)
+      throw err
+   else
+      console.log('Mysql terkoneksi')
+}) 
+
+module.exports = conn
+
+
